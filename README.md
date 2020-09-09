ff484b90529e919bec503ac43b15420d0fcddaef
Edenik <Edenik5@gmail.com>
2020-09-10 01:33:37 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Food Delivery App UI
+
+![Finished App](https://github.com/Edenik/Images/blob/master/Food%20Delivery%20UI.gif)
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Delivery',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey[50],
+        primarySwatch: Colors.deepOrange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
